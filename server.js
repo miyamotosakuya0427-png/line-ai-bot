2b4300399e1152eb74c0083e04798fb15bb177d3
miyamotosakuya0427-png <miyamotosakuya0427@gmail.com>
2026-09-23 09:32:26 +0900

    server.js を更新

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ async function getAIReply(message) {
 
   console.log("OpenAI RESPONSE:", JSON.stringify(data));
 
-   return data.output?.[0]?.content?.[0]?.text || "回答を取得できませんでした。";
+   return data.output[0].content[0].text;
 
 function verifySignature(body, signature) {
   const hash = crypto
